24dd6793a361dc52ced908ddb2c1175c145382e1
rudibroekhuizen <rudi.broekhuizen@naturalis.nl>
2014-09-09 11:11:12 +0200

    Update hardware-add.pp

--- manifests/hardware-add.pp
+++ manifests/hardware-add.pp
@@ -53,8 +53,8 @@ class topdesk::hardware-add (
                 --data-urlencode 'field8=ipadres' \
                 --data-urlencode 'value8=${ipaddress}' \
                 \
-                --data-urlencode 'save=true' " \
-                > /opt/hardware-add.log,
+                --data-urlencode 'save=true' \
+                > /opt/hardware-add.log",
     path      => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
     cwd       => /opt,
     logoutput => true,
